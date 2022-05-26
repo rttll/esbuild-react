e92d1be543b7802f0e71487ecfa400b6fd50d9fd
Adro <acousens@gmail.com>
2022-05-25 21:19:15 -0700

    init

---
+++ app/javascript/application.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+  return <h1>Hellooooo</h1>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App />);
